7637f6bd4d502fc119dd8d7cc4c7d23f4c039dbe
tcsirvinedjimenez <39862976+tcsirvinedjimenez@users.noreply.github.com>
2018-06-03 01:15:12 -0700

    fix

--- pintest.c
+++ pintest.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-// LED Pin - wiringPi pin 0 is BCM_GPIO 17.
 
-#define	LED  15
+#define	LED	15
 #define SWITCH 4
-#define LIGHT 16
+#define LIGHT 16 
 
 int main (void)
 {
@@ -13,25 +12,23 @@ int main (void)
   
   wiringPiSetup () ;
   pinMode (LED, OUTPUT) ;
-  pinMode (LIGHT, INPUT);
-  //pinMode (SWITCH, INPUT) ;
-  pullUpDnControl(LIGHT, PUD_DOWN);
+  pinMode (LIGHT, INPUT) ;
+  //pullUpDnControl(SWITCH, PUD_UP);
 
   while(1)
   {
-    //if(digitalRead(LIGHT)){
-      //digitalWrite (LED, HIGH) ;// On
-    //}else{
-      //digitalWrite (LED, LOW) ;	// Off
-    //}
-	if(digitalRead(LIGHT)){
-		printf("on \n");
-		delay(1000);
-	}
-	else{
-		printf("off \n");
-		delay(1000);
-	}	
+    int count = 0;
+    wiringPiSetup () ;
+    pinMode (LIGHT, OUTPUT) ;
+    digitalWrite(LIGHT,LOW);
+    delay(100);
+
+    pinmode(LIGHT,INPUT);
+
+    while(!ditigalRead(LIGHT)){
+      count += 1;
+    }
+    printf("count: %d \n",count);
   }
   return 0 ;
 }
