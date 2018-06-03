746690f8f6cf813b6fb269c7ce6faea6c6bc33c6
tcsirvinedjimenez <39862976+tcsirvinedjimenez@users.noreply.github.com>
2018-06-02 22:11:44 -0700

    check only when mail is updated

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #define A3 pinbase + 3
 
 int Check_In_Email(char *str);
+int Mail_Is_Diffrent(void);
  
 int main(int argc, char** argv)
 {
@@ -20,34 +21,65 @@ int main(int argc, char** argv)
 	char temp[256];
 	char temp2[256];
 	float temperature=0.0;
+	
+	system("su - pi -c \"rm /var/temp/mail\"");
 	while(1){
 		system("su - pi -c \"fetchmail > /dev/null\"");
-		//check for txt from email
-		check[0]=Check_In_Email(emailname);
-		if(check[0]==-1){
-			printf("File Error 0\n");
+		if(Mail_Is_Diffrent()){
+			//check for txt from email
+			check[0]=Check_In_Email(emailname);
+			if(check[0]==-1){
+				printf("File Error 0\n");
+			}
+			else if(check[0]){
+				printf("Message Recieved\n");
+			}
+			//check for temp command
+			check[1]=Check_In_Email(strtemp);
+			if(check[1]==-1){
+				printf("File Error 1\n");
+			}
+			else if(check[1]){
+				strcpy(temp, "echo \"Rasberry Pi\" | mail -s \"Temperature is ");
+				gcvt(temperature, 6, temp2);
+				strcat(temp,temp2);
+				strcat(temp, "\" dannyjim2019@gmail.com");
+				//printf("%s\n",temp);
+				system(temp);
+			}
 		}
-		else if(check[0]){
-			printf("Message Recieved\n");
-		}
-		//check for temp command
-		check[1]=Check_In_Email(strtemp);
-		if(check[1]==-1){
-			printf("File Error 1\n");
-		}
-		else if(check[1]){
-			strcpy(temp, "echo \"Rasberry Pi\" | mail -s \"Temperature is ");
-			gcvt(temperature, 6, temp2);
-			strcat(temp,temp2);
-			strcat(temp, "\" dannyjim2019@gmail.com");
-			//printf("%s\n",temp);
-			system(temp);
-		}
+		system("su - pi -c \"cp /var/tmp/mail /var/tmp/mailcopy\"");
 	}
    
    return 0;
 }
-
+int Mail_Is_Diffrent(void){
+	FILE *fp1;
+	FILE *fp2;
+	char temp1[512];
+	char temp2[512];
+	if((fp1 = fopen("/var/tmp/mail", "r")) == NULL) {
+		return(-1);
+	}
+	if((fp2 = fopen("/var/tmp/mailcopy", "r")) == NULL) {
+		return(-1);
+	}
+	while((fgets(temp1, 512, fp1) != NULL) && (fgets(temp2, 512, fp2) != NULL)) {
+		if(temp1!=temp2) {
+			return 1;
+		}
+	}
+	
+	if(fp1) {
+		fclose(fp1);
+	}
+	if(fp2) {
+		fclose(fp2);
+	}
+	return 0;
+	
+	
+}
 int Check_In_Email(char *str){
 	FILE *fp;
 	int count = 0;
@@ -57,7 +89,7 @@ int Check_In_Email(char *str){
 		return(-1);
 	}
 	while(fgets(temp, 512, fp) != NULL) {
-		printf("%s",temp);
+		//printf("%s",temp);
 		if((strstr(temp, str)) != NULL) {
 			count++;
 		}
