90b25014bf96251f69d27b42ee1cfac3c572c00d
tcsirvinedjimenez <39862976+tcsirvinedjimenez@users.noreply.github.com>
2018-06-08 08:51:00 -0700

    fix

--- final.c
+++ final.c
@@ -131,29 +131,24 @@ int main(int argc, char** argv)
 				
 				delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
 				newDelta=(int)delta_us;
-				printf("%d - 1000=%d  <%d\n",oldDelta,oldDelta-1000,newDelta);
-				printf("%d + 1000=%d > %d\n",oldDelta,oldDelta+1000,newDelta);
+				//printf("%d - 1000=%d  <%d\n",oldDelta,oldDelta-1000,newDelta);
+				//printf("%d + 1000=%d > %d\n",oldDelta,oldDelta+1000,newDelta);
 				//printf("count: %d\n",count);
-				if(startf!=0 && count>100000){
-					oldDelta=oldDelta-2000;
-					printf("%d > %d\n",newDelta,oldDelta);
-					if(newDelta > oldDelta){
-						oldDelta=oldDelta+4000;
-						printf("%d < %d\n",newDelta,oldDelta);
-						if(newDelta < oldDelta){
-							strcpy(buffer1,"             ");
-							lcdPosition(lcd, 0, 0);
-							strcpy(buffer1,"             ");
-							lcdPosition(lcd, 0, 1);
-							strcpy(buffer1,"Intruder Detected");
-							lcdPuts(lcd, buffer1);
-							lcdPosition(lcd, 0, 1);
-							digitalWrite (LED2, HIGH) ;
-							digitalWrite (BUZZ, HIGH) ;
-							system("echo \"Rasberry Pi\" | mail -s \"Intruder detected Turn off Alarm?\" 6192194457@pm.sprint.com");
-							intruder=1;
-						}
-					}
+				if(startf!=0 && (count>100000|| newDelta < oldDelta-2000||newDelta > oldDelta+2000)){
+					
+					strcpy(buffer1,"             ");
+					lcdPosition(lcd, 0, 0);
+					strcpy(buffer1,"             ");
+					lcdPosition(lcd, 0, 1);
+					strcpy(buffer1,"Intruder Detected");
+					lcdPuts(lcd, buffer1);
+					lcdPosition(lcd, 0, 1);
+					digitalWrite (LED2, HIGH) ;
+					digitalWrite (BUZZ, HIGH) ;
+					system("echo \"Rasberry Pi\" | mail -s \"Intruder detected Turn off Alarm?\" 6192194457@pm.sprint.com");
+					intruder=1;
+						
+					
 				}
 				startf=1;
 				oldDelta=newDelta;
