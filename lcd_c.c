9ab481fdb735b53298be85fc2ed9d5a22f042080
Abel <abelj1@uci.edu>
2018-06-08 00:04:54 -0700

    fix2

--- lcd_c.c
+++ lcd_c.c
@@ -30,10 +30,10 @@ void main(void) {
 	pinMode(RS, OUTPUT);
 	pinMode(E, OUTPUT);
 
-	functionSet();
-	entryModeSet(1, 0); // increment and no shift
+	//functionSet();
+	//entryModeSet(1, 0); // increment and no shift
 	displayOnOffControl(1, 1, 1); // display on, cursor on and blinking on
-	setDdRamAddress(0);
+	//setDdRamAddress(0);
 	sendString("test");
 	//setDdRamAddress(0x40); // set address to start of second line
 	//sendString("Based on Hitachi HD44780");
