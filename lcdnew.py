d04de53a62478cf371480cbc911e58b54cb30bb2
tcsirvinedjimenez <39862976+tcsirvinedjimenez@users.noreply.github.com>
2018-06-07 18:57:24 -0700

    fix

--- lcdnew.py
+++ lcdnew.py
@@ -1,3 +1,3 @@
-from RPLCD import CharLCD
-lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+from RPLCD-0.4.0 import CharLCD
+lcd = CharLCD(cols=16, rows=2, pin_rs=26, pin_e=19, pins_data=[13, 6, 5, 11])
 lcd.write_string(u'Hello world!')
