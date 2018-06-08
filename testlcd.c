c075badc70f5a5937c4b2281659f49f3ad041a06
tcsirvinedjimenez <39862976+tcsirvinedjimenez@users.noreply.github.com>
2018-06-08 04:00:41 -0700

    fic

--- testlcd.c
+++ testlcd.c
@@ -6,12 +6,12 @@
 
 //Pin numbers below are the WiringPi pin numbers
 
-#define LCD_RS  11               //Register select pin
-#define LCD_E   10               //Enable Pin
-#define LCD_D4  6               //Data pin 4
-#define LCD_D5  5               //Data pin 5
-#define LCD_D6  4               //Data pin 6
-#define LCD_D7  1               //Data pin 7
+#define LCD_RS  14               //Register select pin
+#define LCD_E   21              //Enable Pin
+#define LCD_D4  22               //Data pin 4
+#define LCD_D5  23               //Data pin 5
+#define LCD_D6  24               //Data pin 6
+#define LCD_D7  25               //Data pin 7
 
 int main()
 {
