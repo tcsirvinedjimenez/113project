4e820a52b258db18ef82533b369b9cdb9bafa5c0
tcsirvinedjimenez <39862976+tcsirvinedjimenez@users.noreply.github.com>
2018-06-07 23:33:19 -0700

    fix

--- lcdnew.c
+++ lcdnew.c
@@ -2,12 +2,12 @@
 #include <lcd.h>               
  
 //USE WIRINGPI PIN NUMBERS
-#define LCD_RS  27               //Register select pin
-#define LCD_E   22               //Enable Pin
-#define LCD_D4  25              //Data pin 4
-#define LCD_D5  24               //Data pin 5
-#define LCD_D6  23               //Data pin 6
-#define LCD_D7  18               //Data pin 7
+#define LCD_RS  14               //Register select pin
+#define LCD_E   21               //Enable Pin
+#define LCD_D4  22              //Data pin 4
+#define LCD_D5  23               //Data pin 5
+#define LCD_D6  24               //Data pin 6
+#define LCD_D7  25               //Data pin 7
  
 int main()
 {
