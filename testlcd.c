6cce140c9c8107c5fc3321a674fd57053c2a4a00
Abel <abelj1@uci.edu>
2018-06-08 00:17:19 -0700

    added new test

---
+++ testlcd.c
@@ -0,0 +1,19 @@
+#include <wiringPi.h>          
+#include <lcd.h>               
+ 
+//USE WIRINGPI PIN NUMBERS
+#define LCD_RS  14               //Register select pin
+#define LCD_E   21               //Enable Pin
+#define LCD_D4  22               //Data pin 4
+#define LCD_D5  23               //Data pin 5
+#define LCD_D6  24               //Data pin 6
+#define LCD_D7  25               //Data pin 7
+ 
+int main()
+{
+    int lcd;               
+    wiringPiSetup();        
+    lcd = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+               
+    lcdPuts(lcd, "Hello, world!");   
+}
