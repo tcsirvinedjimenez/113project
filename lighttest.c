21c7e8d6bf4f0dc2aa5aebef188b6fdc6c7ea4cf
tcsirvinedjimenez <39862976+tcsirvinedjimenez@users.noreply.github.com>
2018-06-08 04:51:08 -0700

    added led test

---
+++ lighttest.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+
+#define	LED	15
+#define SWITCH 4
+#define LIGHT 16 
+
+int main (void)
+{
+  printf ("Raspberry Pi blink\n") ;
+  
+  wiringPiSetup () ;
+  pinMode (LED, OUTPUT) ;
+  pinMode (LIGHT, INPUT) ;
+  //pullUpDnControl(SWITCH, PUD_UP);
+
+  while(1)
+  {
+    int count = 0;
+    wiringPiSetup () ;
+    pinMode (LIGHT, OUTPUT) ;
+    digitalWrite(LIGHT,LOW);
+    delay(100);
+
+    pinmode(LIGHT,INPUT);
+
+    while(!ditigalRead(LIGHT)){
+      count += 1;
+    }
+    printf("count: %d \n",count);
+  }
+  return 0 ;
+}
