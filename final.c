78e459ca29e1063c5627028820986b8c14e238ad
tcsirvinedjimenez <39862976+tcsirvinedjimenez@users.noreply.github.com>
2018-06-08 07:04:04 -0700

    fix

--- final.c
+++ final.c
@@ -122,7 +122,7 @@ int main(int argc, char** argv)
 				delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
 				printf("Distance: %f\n",delta_us);
 				printf("%d\n",count);
-				printf('%f\n',delta_us-2500.0);
+				printf('%d\n',delta_us-2500.0);
 				if(count>100000 || ((oldDelta>delta_us-2500.0) && (oldDelta<delta_us+2500.0))){
 					strcpy(buffer1,"             ");
 					lcdPosition(lcd, 0, 0);
